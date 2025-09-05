640216601866335971e5441139a3997af04c6a46
ebinee <skyim0907@naver.com>
2025-09-05 16:04:16 +0900

    infra: edit s3 config in  application-test.yml

--- back/src/test/java/com/springboot/be/BeApplicationTests.java
+++ back/src/test/java/com/springboot/be/BeApplicationTests.java
@@ -3,10 +3,18 @@ package com.springboot.be;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class BeApplicationTests {
+
+    @MockitoBean
+    S3Client s3Client;
+    @MockitoBean
+    S3Presigner s3Presigner;
 
     @Test
     void contextLoads() {
